57d42cc09b352d9b5cca54695d2d21b017f8916f
Konstantin <kpozdnikin@gmail.com>
2019-04-06 13:54:09 +0300

    list tests

--- app/components/List/tests/index.test.js
+++ app/components/List/tests/index.test.js
@@ -1,67 +1,57 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { mount, configure } from 'enzyme';
+import { fromJS } from 'immutable';
+import Adapter from 'enzyme-adapter-react-16';
+import { Provider } from 'react-redux';
+import configureStore from 'redux-mock-store';
 import { act } from 'react-dom/test-utils';
-// import { shallow } from 'enzyme';
 import Table from '@material-ui/core/Table';
+import TextField from '@material-ui/core/TextField';
+import { initialState as homeReducerState } from '../../../containers/HomePage/reducer';
 import List from '../index';
 import items from '../../../tests/flights';
 
+configure({ adapter: new Adapter() });
+
 describe('<List />', () => {
-  let container;
+  const middlewares = [];
+  const mockStore = configureStore(middlewares);
+  let store;
 
-  beforeEach(() => {
-    container = document.createElement('div');
-    document.body.appendChild(container);
-  });
-
-  afterEach(() => {
-    document.body.removeChild(container);
-    container = null;
-  });
-
-  it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<List items={items} />, div);
-    ReactDOM.unmountComponentAtNode(div);
+  beforeAll(() => {
+    const initialState = fromJS({
+      home: homeReducerState,
+    });
+    store = mockStore(initialState);
   });
 
   it('can render and update a counter', () => {
     // Test first render and effect
     act(() => {
-      ReactDOM.render(<List items={items} />, container);
+      const renderedComponent = mount(
+        <Provider store={store}>
+          <List items={items} />
+        </Provider>,
+      );
+      expect(renderedComponent.find(TextField).length).toBe(1);
+      expect(renderedComponent.find(Table).length).toBe(0);
     });
-    const table = container.querySelector(Table);
-    expect(table.textContent).toBe('123');
-
-    /* const button = container.querySelector('button');
-    const label = container.querySelector('p');
-    expect(label.textContent).toBe('You clicked 0 times');
-    expect(document.title).toBe('You clicked 0 times');
-
-    // Test second render and effect
-    act(() => {
-      button.dispatchEvent(new MouseEvent('click', {bubbles: true}));
-    });
-    expect(label.textContent).toBe('You clicked 1 times');
-    expect(document.title).toBe('You clicked 1 times');
-  */
   });
 
-  /* it('should render the component if no items are passed', () => {
-    const renderedComponent = shallow(<List items={items} />);
-    expect(renderedComponent.find(Table)).toBeDefined();
-  }); */
-
-  /* it('should pass all items props to rendered component', () => {
-    const items = [{ id: 1, name: 'Hello' }, { id: 2, name: 'World' }];
-
-    const component = ({ item }) => <ListItem>{item.name}</ListItem>; // eslint-disable-line react/prop-types
-
-    const renderedComponent = shallow(
-      <List items={items} component={component} />
-    );
-    expect(renderedComponent.find(component)).toHaveLength(2);
-    expect(renderedComponent.find(component).at(0).prop('item')).toBe(items[0]);
-    expect(renderedComponent.find(component).at(1).prop('item')).toBe(items[1]);
-  }); */
+  it('can render and update a counter', () => {
+    // Test first render and effect
+    const newItems = {
+      cheap: items.cheap,
+      business: items.business,
+      all: items.cheap,
+    };
+    act(() => {
+      const renderedComponent = mount(
+        <Provider store={store}>
+          <List items={newItems} />
+        </Provider>,
+      );
+      expect(renderedComponent.find(Table).length).toBe(1);
+    });
+  });
 });
